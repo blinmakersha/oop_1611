b0ae14ccffd8a0d1327411fba39ae3604a4e03f5
wisezeee <gafiatullinakarina26@gmail.com>
2022-11-16 22:43:03 +0300

    tests

--- test_sphere.py
+++ test_sphere.py
@@ -1,24 +1,28 @@
-from sphere2 import Sphere, MoveUni
+from main import Sphere
 import pytest
 
-test_get_radius = [(10, 10), (20, 20), (30, 30)]
+tests_sphere_init = [(4, 4, 4, 4)]
 
-test_uniform_motion = [(10, 4, 6, 137.51), (50, 20, 10, 229.18), (100, 0, 10, 0.0)]
+test_uniform_motion = [(10, 4, 2, 6, 137.51), (50, 20, 2, 10, 229.18), (100, 0, 2, 10, 0.0)]
 
-test_acc_motion = [(100, 0, 10, 0.0), (10, 3, 10, 1718.87), (100, 30, 50, 85943.67)]
+test_acc_motion = [(100, 0, 0, 10, 0.0), (10, 0, 3, 10, 1718.87), (100, 0, 30, 50, 85943.67)]
 
 
-@pytest.mark.parametrize('radius_valid, radius_expected', test_get_radius)
-def test_getRadius(radius_valid, radius_expected):
-    ball = Sphere(radius_valid)
-    assert ball.get_radius() == radius_expected
+@pytest.mark.parametrize('radius, speed, boost, time', tests_sphere_init)
+def test_init_ball(radius, speed, boost, time):
+    ball = Sphere(radius, speed, boost, time)
+    assert ball.radius == radius and ball.speed == speed and ball.boost == boost and ball.time == time
 
-@pytest.mark.parametrize('radius, speed, time, grade', test_uniform_motion)
-def test_uniformMotion(radius, speed, time, grade):
-    res = MoveUni.uniform_motion(radius, speed, time)
+
+@pytest.mark.parametrize('radius, speed,boost, time, grade', test_uniform_motion)
+def test_uniformmotion(radius, speed, boost, time, grade):
+    ball = Sphere(radius, speed, boost, time)
+    res = ball.uniform_motion()
     assert res == grade
 
-@pytest.mark.parametrize('radius, boost, time, grade', test_acc_motion)
-def test_accMotion(radius, boost, time, grade):
-    res = MoveUni.acc_motion(radius, boost, time)
+
+@pytest.mark.parametrize('radius,speed, boost, time, grade', test_acc_motion)
+def test_accmotion(radius, speed, boost, time, grade):
+    ball = Sphere(radius, speed, boost, time)
+    res = ball.acc_motion()
     assert res == grade
