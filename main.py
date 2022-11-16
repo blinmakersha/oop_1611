136a844617b008a3590ef9d43b30acfac7137aeb
blinmakersha <valentinakulg@gmail.com>
2022-11-16 19:50:05 +0300

    Добавлен главный файл.

---
+++ main.py
@@ -0,0 +1,46 @@
+"""File with class Sphere."""
+from math import pi
+
+alpha = 180
+
+
+class Sphere:
+    """Class for the Sphere."""
+
+    def __init__(self, radius=1, speed=0, boost=0, time=0):
+        """Creates a sphere.
+
+        Args:
+            radius: float - radius of sphere.
+            speed: float - speed of the sphere.
+            boost: float - acceleration of the ball.
+            time: float - time when sphere are rolling.
+        """
+        self.radius = radius
+        self.speed = speed
+        self.boost = boost
+        self.time = time
+
+    def uniform_motion(self):
+        """Function finds angle of the uniform motioned by speed sphere.
+
+        Returns:
+            angle: float - angle in degrees.
+        """
+        way = self.speed * self.time
+        res = (way * alpha) / (pi * self.radius)
+        return round(res, 2)
+
+    def acc_motion(self):
+        """Function finds angle of the uniformly accelerated sphere.
+
+        Returns:
+            angle: float - angle in degrees.
+        """
+        speed = self.boost * self.radius
+        way = speed * self.time
+        res = (way * alpha) / (pi * self.radius)
+        return round(res, 2)
+
+# print(Sphere(9, 5, 2, 4).uniform_motion())
+# print(Sphere(9, 5, 2, 4).acc_motion())
